b2b1ebbc37e4ff435bc5c2133b523631de0251ed
insafmechergui <insaf_mechergui@outlook.com>
2021-04-30 23:22:35 +0100

    save data to array

--- medical-record/src/component/patient/record/InformationsGenerales.js
+++ medical-record/src/component/patient/record/InformationsGenerales.js
@@ -1,7 +1,7 @@
 import React, { Component} from 'react'
 import Button from '@material-ui/core/Button';
 import {FormControl,Input,NativeSelect,FormLabel,FormControlLabel,Radio  } from '@material-ui/core'
-
+import dataPatient from '../../../patient.json'
 
 export default class InformationsGenerales extends Component {
     constructor(){
@@ -15,60 +15,83 @@ export default class InformationsGenerales extends Component {
             profession:'',
             phone:'',
             dateSick:'',
-
+            patients:[]
 
         }
     }
+    onChange = e => {
+        this.setState({ 
+            [e.target.name]: e.target.value 
+        });
+    };
+
+    onSubmit = e => {
+        e.preventDefault()
+        const patient = {
+            cin : this.state.cin,
+            nom : this.state.nom,
+            prenom : this.state.prenom,
+            dateNaissance : this.state.dateNaissance,
+            adresse : this.state.adresse,
+            profession : this.state.profession,
+            phone : this.state.phone,
+            dateSick : this.state.dateSick,
+        }
+         this.setState({
+           patients:this.state.patients.concat(patient)
+          })
+        }
     render() {
+        console.log('pattient',this.state.patients)
         return (
             <div id="generalInformation">
-                <form id="addPatientForm" className="generalInformationForm" class="form">
+                <form id="addPatientForm" className="generalInformationForm" class="form" onSubmit={this.onSubmit}>
                     <div class="field">
                         <label class="label">CIN</label>
                         <div class="control">
-                            <input class="input" required placeholder="CIN" type="number" name='cin' />
+                            <input class="input" required placeholder="CIN" type="number" name='cin' value={this.state.cin} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Nom</label>
                         <div class="control">
-                            <input class="input" required placeholder="Name" type="text" name='nom'/>
+                            <input class="input" required placeholder="Name" type="text" name='nom' value={this.state.nom} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Prenom</label>
                         <div class="control">
-                            <input class="input" required placeholder="prenom" type="text" name='prenom'/>
+                            <input class="input" required placeholder="prenom" type="text" name='prenom' value={this.state.prenom} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Date de naissance</label>
                         <div class="control">
-                            <input class="input" type="date" name='dateNaissance'/>
+                            <input class="input" type="date" name='dateNaissance' value={this.state.dateNaissance} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Adresse</label>
                         <div class="control">
-                            <input class="input"  placeholder="adresse" type="text" name='adresse'/>
+                            <input class="input"  placeholder="adresse" type="text" name='adresse' value={this.state.adresse} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Profession</label>
                         <div class="control">
-                            <input class="input" placeholder="profession" type="text" name='profession'/>
+                            <input class="input" placeholder="profession" type="text" name='profession'value={this.state.profession} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Telephone</label>
                         <div class="control">
-                            <input class="input" placeholder="Phone" type="number" name='phone'/>
+                            <input class="input" placeholder="Phone" type="number" name='phone' value={this.state.phone} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Date de debut de maladie</label>
                         <div class="control">
-                            <input class="input" type="date" name='dateSick'/>
+                            <input class="input" type="date" name='dateSick' value={this.state.dateSick} onChange={this.onChange}/>
                         </div>
                     </div>
                     <div class="field">
